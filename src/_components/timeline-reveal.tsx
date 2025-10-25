299af260ba8793645270dc43ab778c4550de05ac
Jonatan Missora <jonatanjmissora@gmail.com>
2025-10-25 16:38:57 -0300

    listo lenis

--- src/_components/timeline-reveal.tsx
+++ src/_components/timeline-reveal.tsx
@@ -6,47 +6,47 @@ export default function TimelineReveal() {
 
             <div className="w-3/4 mx-auto flex flex-col gap-8 h-[800px] relative">
 
-                <div className="w-full h-[150px] flex items-center justify-center gap-20 fadeUp">
-                    <div className="w-[40%]">
-                        <img className="ml-auto" src="https://picsum.photos/100/150" width="100" height="150" alt="foto1" />
+                <div className="w-full h-[150px] flex items-center justify-center gap-30 fadeUp">
+                    <div className="w-[400px]">
+                        <img className="ml-auto" src="https://picsum.photos/300/150" width="300" height="150" alt="foto1" />
                     </div>
-                    <div className="min-w-[100px] max-w-[40%]">
+                    <div className="w-[400px]">
                         <p className="font-bold">2022</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                        <p className="w-1/2">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
                         </div>
                 </div>
 
-                <div className="w-full h-[150px] flex items-center justify-center gap-20 fadeUp">
-                    <div className="min-w-[100px] max-w-[40%] text-right">
+                <div className="w-full h-[150px] flex items-center justify-center gap-30 fadeUp">
+                    <div className="w-[400px] text-right">
                         <p className="font-bold">2021</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                        <p className="w-1/2 ml-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
                         </div>
-                    <div className="w-[40%]">
-                        <img className="mr-auto" src="https://picsum.photos/100/151" width="100" height="150" alt="foto1" />
+                    <div className="w-[400px]">
+                        <img className="mr-auto" src="https://picsum.photos/300/151" width="300" height="150" alt="foto1" />
                     </div>
                 </div>
 
-                <div className="w-full h-[150px] flex items-center justify-center gap-20 fadeUp">
-                    <div className="w-[40%]">
-                        <img className="ml-auto" src="https://picsum.photos/100/152" width="100" height="150" alt="foto1" />
+                <div className="w-full h-[150px] flex items-center justify-center gap-30 fadeUp">
+                    <div className="w-[400px]">
+                        <img className="ml-auto" src="https://picsum.photos/300/152" width="300" height="150" alt="foto1" />
                     </div>
-                    <div className="min-w-[100px] max-w-[40%]">
+                    <div className="w-[400px]">
                         <p className="font-bold">2020</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                        <p className="w-1/2">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
                         </div>
                 </div>
 
-                <div className="w-full h-[150px] flex items-center justify-center gap-20 fadeUp">
-                    <div className="min-w-[100px] max-w-[40%] text-right">
+                <div className="w-full h-[150px] flex items-center justify-center gap-30 fadeUp">
+                    <div className="w-[400px] text-right">
                         <p className="font-bold">2019</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+                        <p className="w-1/2 ml-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
                         </div>
-                    <div className="w-[40%]">
-                        <img className="mr-auto" src="https://picsum.photos/100/153" width="100" height="150" alt="foto1" />
+                    <div className="w-[400px]">
+                        <img className="mr-auto" src="https://picsum.photos/300/153" width="300" height="150" alt="foto1" />
                     </div>
                 </div>
 
-                <div className="absolute top-0 bottom-0 left-[49%] bg-blue-500 w-[2px]">
+                <div className="absolute top-0 bottom-0 left-[49.9%] bg-blue-500 w-[2px]">
 
                 </div>
 
